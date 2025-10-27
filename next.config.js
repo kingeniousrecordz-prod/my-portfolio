a7d1832d6adfa5e9adc0e3deb4456af17b4cca68
kingeniousrecordz-prod <kingeniousrecordz@gmail.com>
2025-10-27 03:30:52 +0000

    Add vercel.json and simplify next.config.js to fix build trace error

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
   images: {
     domains: ['localhost'],
   },
+  swcMinify: true,
 }
 
 module.exports = nextConfig
